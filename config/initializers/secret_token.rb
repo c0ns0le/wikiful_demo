465a247dd1a0e8cb00b2742793c4345913b071e3
Benjamin White <benjamin.e.white1@gmail.com>
2013-11-12 12:17:34 -0500

    initialized new wikiful_demo rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WikifulDemo::Application.config.secret_key_base = '1420fc87de7957f72d70f2a736a8233184fcd8dfbb7205dd977e4f1f1eb133c2a1fd3cfdaa2afecd7d9923b57ad32d21c3ef059ce7ea250fe5e3823c7920a927'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WikifulDemo::Application.config.session_store :cookie_store, key: '_wikiful_demo_session'
